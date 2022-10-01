e57a85000f75c7702c795f9c9a721d9d5087e2c4
Iannealer <duwjdtn@ajou.ac.kr>
2022-10-01 16:19:11 +0900

    DB 연결 및 환경변수 초기화

--- go-jwt/main.go
+++ go-jwt/main.go
@@ -1,1 +1,25 @@
-package go_jwt
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_playground/go-jwt/initializers"
+	"net/http"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectDB()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
