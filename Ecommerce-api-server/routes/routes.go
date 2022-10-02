eaa41b997e8b694f5d3ab93f1da8dec443c165f8
Iannealer <duwjdtn@ajou.ac.kr>
2022-10-03 02:25:24 +0900

    Feat: Routing 명세 정의

--- Ecommerce-api-server/routes/routes.go
+++ Ecommerce-api-server/routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/iannealer/go_playground/Ecommerce-api-server/controllers"
+)
+
+func UserRoutes(route *gin.Engine) {
+	route.POST("/users/signup", controllers.SignUp())
+	route.POST("/users/login", controllers.Login())
+	route.POST("/admin/addProduct", controllers.ProductViewerAdmin())
+	route.GET("/users/productView", controllers.SearchProduct())
+	route.GET("/users/search", controllers.SearchProductByQuery())
+}
